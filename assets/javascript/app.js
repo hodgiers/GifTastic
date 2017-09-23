462945212b0bb95a657906b8aa2d2c0e38007a44
Rashan Hodge <rashan.hodge@gmail.com>
2017-09-23 05:17:29 -0400

    Beginning basic logic coding and partial pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+
+
+// Create an array of strings, each one related to a topic that interests me
+
+var topics = ["hack", "raspberry pi", "wifi", "3dprinting"]
+
+// Save the array to a variable called 'topics'
+
+//Take the topics in the array and create buttons in the HTML
+	//Consider using a loop that appends a button for each string in the array
+
+//When user clicks a button, page should grab 10 static, non-animated gif images from the 
+//Giphy api and place them on the page.
+
+//When user clicks one of the still GIPHY images, the GIF should animate.
+//If user clicks again, the animation should stop.
+
+//Display rating under every GIF (rating provided by API)
+
+//Include form on page that takes user input box and adds it to the topics array.
+//Then make a function call that adds each topic as a button option.
+
